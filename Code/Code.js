31b3c730d369487e332cc504433cf880ff9993f6
Sundar Solai <sundar@Sundars-MacBook-Pro.local>
2018-05-17 09:34:24 -0400

    Added Sundar's name

--- Code/Code.js
+++ Code/Code.js
@@ -1,3 +1,4 @@
 function myFunction() {
-  SpreadsheetApp.getActiveSheet().getRange("A1").setValue("Hello World!")
+  SpreadsheetApp.getActiveSheet().getRange("A1").setValue("Hello World!");
+  SpreadsheetApp.getActiveSheet().getRange("A2").setValue("Sundar");
 }
