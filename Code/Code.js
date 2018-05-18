6da789d25f18292fd7080ea67d99262b5e7a734d
Ryan Kim <ryankim@college.harvard.edu>
2018-05-18 21:33:05 +0000

    ryan update

--- Code/Code.js
+++ Code/Code.js
@@ -1,4 +1,5 @@
 function myFunction() {
   SpreadsheetApp.getActiveSheet().getRange("A1").setValue("Hello World!");
   SpreadsheetApp.getActiveSheet().getRange("A2").setValue("Sundar sundarnj");
+  SpreadsheetApp.getActiveSheet().getRange("A3").setValue("Ryan p0ssibl3");
 }
