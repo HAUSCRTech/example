41dc456f9126824f8e65f0dbb5947aa202cef7c0
Linda Qin <qinx@college.harvard.edu>
2018-05-18 17:58:09 -0400

    added linda

--- Code/Code.js
+++ Code/Code.js
@@ -2,4 +2,5 @@ function myFunction() {
   SpreadsheetApp.getActiveSheet().getRange("A1").setValue("Hello World!");
   SpreadsheetApp.getActiveSheet().getRange("A2").setValue("Sundar sundarnj");
   SpreadsheetApp.getActiveSheet().getRange("A3").setValue("Ryan p0ssibl3");
+  SpreadsheetApp.getActiveSheet().getRange("A4").setValue("Linda l1nda");
 }
