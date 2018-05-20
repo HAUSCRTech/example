25269a443bc7cddd8a8d718f572a25bbeb56db20
AllisonKZhang <allison.kangrong.zhang@gmail.com>
2018-05-20 14:36:31 -0400

    added allison

--- Code/Code.js
+++ Code/Code.js
@@ -3,4 +3,6 @@ function myFunction() {
   SpreadsheetApp.getActiveSheet().getRange("A2").setValue("Sundar sundarnj");
   SpreadsheetApp.getActiveSheet().getRange("A3").setValue("Ryan p0ssibl3");
   SpreadsheetApp.getActiveSheet().getRange("A4").setValue("Linda l1nda");
+  SpreadsheetApp.getActiveSheet().getRange("A5").setValue("Amal amalmattoo");  
+  SpreadsheetApp.getActiveSheet().getRange("A5").setValue("Allison AllisonKZhang");
 }
